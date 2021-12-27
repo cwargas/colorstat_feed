995c638f9781fa1eb1e7c2687749a0e294bc8c1f
Chris James <cw51680@gmail.com>
2021-12-27 17:39:31 -0500

    Various style changes

--- downloader.py
+++ downloader.py
@@ -15,7 +15,7 @@ def get_category(url):
     Returns:
         [str]: Category name
     """
-    if 'nypost.com' in url or 'investing.com' in url:
+    if 'nypost.com' in url or 'investing.com' in url or 'finance.yahoo.com' in url or 'deadline.com' in url:
         return 'business'
     elif 'nationalreview.com' in url or 'newsmax.com' in url or 'theamericanconservative.com' in url:
         return 'politics'
@@ -95,11 +95,13 @@ feed_urls = [
     # business
     'https://nypost.com/opinion/feed/',
     'https://www.investing.com/rss/news.rss',
+    'https://finance.yahoo.com/news/rssindex',
     # politics
     'https://www.nationalreview.com/feed/',
     'https://www.newsmax.com/rss/Politics/1/',
     'https://www.theamericanconservative.com/web-categories/politics/feed/',
-    # world
+    # entertainment
+    'https://deadline.com/feed/',
   
     # space articles
     'https://phys.org/rss-feed/breaking/space-news/',
